05078c454d1a3989cff08b903b45075f9193187c
Samolazoff <samolazov.roman.alex@gmail.com>
2023-10-03 16:05:34 +0300

    FEAT: install and test PUG

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -1,6 +1,12 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { src, dest } = require('gulp');
+const pug = require('gulp-pug');
+
+exports.taskPug = () => {
+  return src('./src/pages/**/*.pug')
+    .pipe(
+      pug({
+        pretty:true
+      })
+    )
+    .pipe(dest('./build'));
+};
